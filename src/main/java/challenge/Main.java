690d13bf26cb9e8a3fffb89d9f0fdd1c3807fa2e
beatrizacbs <beatrizacbs@gmail.com>
2019-02-07 22:54:17 -0300

    q4 pronto

--- src/main/java/challenge/Main.java
+++ src/main/java/challenge/Main.java
@@ -75,12 +75,25 @@ public class Main {
 		Iterator<Jogador> iterator = jogadores.iterator();
 		Jogador jogadorAtual;
 		BigDecimal eurClauseAtual;
+		TreeMap<BigDecimal, String> mapJogadores = new TreeMap<>();
 		while (iterator.hasNext()){
 			jogadorAtual = iterator.next();
 			eurClauseAtual = tranformarParaBigDecimal(jogadorAtual.getEur_release_clause());
-
+			mapJogadores.put(eurClauseAtual, jogadorAtual.getFull_name());
 		}
-		return pegarJogadoresQ4().subList(0, 10);
+		return new ArrayList<>(mapJogadores.descendingMap().values()).subList(0, 10);
+	}
+
+	// Quem são os 10 jogadores mais velhos (use como critério de desempate o campo `eur_wage`)?
+	// (utilize as colunas `full_name` e `birth_date`)
+	public List<String> q5() {
+		return pegarJogadoresQ5();
+	}
+
+	// Conte quantos jogadores existem por idade. Para isso, construa um mapa onde as chaves são as idades e os valores a contagem.
+	// (utilize a coluna `age`)
+	public Map<Integer, Integer> q6() {
+		return separarPorIdade();
 	}
 
 	private BigDecimal tranformarParaBigDecimal(String eur_release_clause) {
@@ -91,23 +104,10 @@ public class Main {
 		}
 	}
 
-	// Quem são os 10 jogadores mais velhos (use como critério de desempate o campo `eur_wage`)?
-	// (utilize as colunas `full_name` e `birth_date`)
-	public List<String> q5() {
-		return pegarJogadoresQ5();
-	}
-
 	private LocalDate transformaParaLocalDate(String s) {
 		String[] dateSeparated = s.split("-");
 		return LocalDate.of(Integer.parseInt(dateSeparated[0]), Integer.parseInt(dateSeparated[1]), Integer.parseInt(dateSeparated[2]));
 	}
-
-	// Conte quantos jogadores existem por idade. Para isso, construa um mapa onde as chaves são as idades e os valores a contagem.
-	// (utilize a coluna `age`)
-	public Map<Integer, Integer> q6() {
-		return separarPorIdade();
-	}
-
 
 	private List<String> getNomesJogadores(List<Pair<String, BigDecimal>> pairs) {
 
@@ -148,51 +148,6 @@ public class Main {
 
 		return getNomesJogadores(new ArrayList<>(jogadores.values()));
 
-	}
-
-
-	private List<String> pegarJogadoresQ4(){
-		String linha;
-		boolean isFirstLine = true;
-		TreeMap<BigDecimal, List<String>> jogadores = new TreeMap<>();
-		BigDecimal salarioAtual;
-		try(BufferedReader bufferedReader = new BufferedReader(new FileReader(csvFile))){
-			while((linha = bufferedReader.readLine()) != null){
-				if(!isFirstLine){
-					String[] linhaSplit = linha.split(comma);
-					if(!linhaSplit[eurReleaseClauseIndex].equals("")){
-						salarioAtual = new BigDecimal(linhaSplit[eurReleaseClauseIndex]);
-						if(!jogadores.containsKey(salarioAtual)){
-							List<String> lista = new ArrayList<>();
-							lista.add(linhaSplit[fullNameIndex]);
-							jogadores.put(salarioAtual, lista);
-						}else{
-							List<String> listaAux = new ArrayList<>(jogadores.get(salarioAtual));
-							listaAux.add(linhaSplit[fullNameIndex]);
-							jogadores.put(salarioAtual, listaAux);
-
-						}
-					}
-
-				}else{
-					isFirstLine = false;
-				}
-			}
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-
-
-		return retirarNomesMaiores(jogadores);
-	}
-
-	private List<String> retirarNomesMaiores(TreeMap<BigDecimal, List<String>> jogadores) {
-
-		List<String> retorno = new ArrayList<>();
-		for (List<String> lista: jogadores.descendingMap().values()) {
-			retorno.addAll(lista);
-		}
-		return retorno;
 	}
 
 	private Map<Integer, Integer> separarPorIdade(){
